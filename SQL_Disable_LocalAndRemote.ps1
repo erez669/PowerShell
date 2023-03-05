c19fd928aca7d2177474e66704e29cd8fb447232
erez669 <57612608+erez669@users.noreply.github.com>
2023-03-05 07:46:45 +0200

    Update SQL_Disable_LocalAndRemote.ps1

--- SQL_Disable_LocalAndRemote.ps1
+++ SQL_Disable_LocalAndRemote.ps1
@@ -4,6 +4,10 @@ $instance2 = "SQL Server Agent (MSSQLSERVER)"
 $instance3 = "World Wide Web Publishing Service"
 $computerName = $env:COMPUTERNAME
 
+$serviceName1 = "MSSQLSERVER"
+$serviceName2 = "SQLSERVERAGENT"
+$serviceName3 = "W3SVC"
+
 Stop-Service -DisplayName $instance1 -Force
 $service1 = Get-WmiObject -Class Win32_Service -Filter "DisplayName='$instance1'"
 $service1.ChangeStartMode("Disabled")
@@ -13,18 +17,18 @@ $service2.ChangeStartMode("Disabled")
 Stop-Service -DisplayName $instance3 -Force
 $service3 = Get-WmiObject -Class Win32_Service -Filter "DisplayName='$instance3'"
 $service3.ChangeStartMode("Disabled")
-Write-Host "The services '$instance1','$instance2' and $instance3 on '$computerName' have been stopped"
+Write-Host "The services '$instance1','$instance2' and $instance3' on '$computerName' have been stopped"
 
 $secondaryServer = 'S' + $computerName.Substring(1)
-$serviceName1 = "MSSQLSERVER"
-$serviceName2 = "SQLSERVERAGENT"
-$serviceName3 = "W3SVC"
-Invoke-Command -ComputerName $secondaryServer -ScriptBlock {
-    Stop-Service -Name $using:serviceName1 -Force
-    Set-Service -Name $using:serviceName1 -StartupType Disabled
-    Stop-Service -Name $using:serviceName2 -Force
-    Set-Service -Name $using:serviceName2 -StartupType Disabled
-    Stop-Service -Name $using:serviceName3 -Force
-    Set-Service -Name $using:serviceName3 -StartupType Disabled
+
+Invoke-Command -ComputerName $secondaryServer -ArgumentList $serviceName1, $serviceName2, $serviceName3 -ScriptBlock {
+    param($serviceName1, $serviceName2, $serviceName3)
+
+    Stop-Service -Name $serviceName1 -Force
+    Set-Service -Name $serviceName1 -StartupType Disabled
+    Stop-Service -Name $serviceName2 -Force
+    Set-Service -Name $serviceName2 -StartupType Disabled
+    Stop-Service -Name $serviceName3 -Force
+    Set-Service -Name $serviceName3 -StartupType Disabled
 }
-Write-Host "The services '$instance1','$instance2' and $instance3 on computer '$secondaryServer' have been stopped"
+Write-Host "The services '$instance1','$instance2' and $instance3' on computer '$secondaryServer' have been stopped"
