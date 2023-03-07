3b5259f060598b306b15814a619076ec52a34da9
erez669 <57612608+erez669@users.noreply.github.com>
2023-03-07 21:34:51 +0200

    Update OneTime_Reboot_2008.ps1

--- OneTime_Reboot_2008.ps1
+++ OneTime_Reboot_2008.ps1
@@ -1,10 +1,12 @@
-﻿$TaskScheduler = New-Object -ComObject "Schedule.Service"
+$TaskScheduler = New-Object -ComObject "Schedule.Service"
 $TaskScheduler.Connect()
 $RootFolder = $TaskScheduler.GetFolder("\")
 $TaskDefinition = $TaskScheduler.NewTask($null)
 $Action = $TaskDefinition.Actions.Create(0)
 $Action.Path = "shutdown.exe"
 $Action.Arguments = "-r -t 0 -f"
+$Action.WorkingDirectory = "C:\Windows\System32"
+$TaskDefinition.Principal.RunLevel = 1 # Highest privileges
 $Trigger = $TaskDefinition.Triggers.Create(1) # time trigger
 $StartTime = [DateTime]::Today.AddDays(1).AddHours(2)
 $Trigger.StartBoundary = $StartTime.ToString("yyyy-MM-ddTHH:mm:ss")
@@ -16,5 +18,4 @@ $Settings.StopIfGoingOnBatteries = $False
 $Settings.WakeToRun = $True
 $Settings.ExecutionTimeLimit = "PT0S"
 $Settings.StartWhenAvailable = $True
-$Settings.IdleSettings = $null
 $RootFolder.RegisterTaskDefinition("Reboot", $TaskDefinition, 6, "NT AUTHORITY\SYSTEM", $null, 5, $null)
