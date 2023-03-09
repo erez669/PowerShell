163b1e3ec05cc126e720ac4c2438271b731c5803
erez669 <57612608+erez669@users.noreply.github.com>
2023-03-09 20:01:54 +0200

    Update and rename ClearSDMCache.ps1 to ClearSDMCache_LocalHost.ps1

---
+++ ClearSDMCache_LocalHost.ps1
@@ -0,0 +1,39 @@
+Clear-Host
+
+$ErrorActionPreference = "SilentlyContinue"
+
+$Folders = @(
+    "D:\sdmcache",
+    "C:\Program Files (x86)\Landesk\ldclient\sdmcache",
+    "C:\Program Files\Landesk\ldclient\sdmcache",
+    "D:\supersol"
+)
+
+Foreach ($Folder in $Folders) {
+    Write-Host -ForegroundColor Yellow "Deleting Folder $Folder on $env:computername"
+    Get-ChildItem -Path $Folder -Recurse -Verbose | Remove-Item -Recurse -Force
+}
+
+# Delete empty folders and subfolders
+Foreach ($Folder in $Folders) {
+    Get-ChildItem $Folder -Directory -Recurse -Verbose `
+        | Sort-Object { $_.FullName.Length } -Descending `
+        | Where-Object { $_.GetFiles().Count -eq 0 } `
+        | ForEach-Object {
+            if ($_.GetDirectories().Count -eq 0) { 
+                Write-Host "Removing $($_.FullName)"
+                $_.Delete()
+            }
+        }
+}
+
+$ExcludeFolder = "D:\supersol"
+
+Foreach ($Folder in $Folders) {
+    if ($Folder -ne $ExcludeFolder) {
+        Write-Host -ForegroundColor Green "Creating Directory $Folder on $env:computername"
+        New-Item -ItemType Directory -Path $Folder -Force
+    } else {
+        Write-Host -ForegroundColor Yellow "Skipping directory $Folder on $env:computername"
+    }
+}
