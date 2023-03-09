fe3bfa84d9f34456aac6084700b895fa8c55d6fe
erez669 <57612608+erez669@users.noreply.github.com>
2023-03-09 22:45:00 +0200

    Update DeleteFolders_SDMCACHE_v3_RemoteServers.ps1

--- DeleteFolders_SDMCACHE_v3_RemoteServers.ps1
+++ DeleteFolders_SDMCACHE_v3_RemoteServers.ps1
@@ -1,19 +1,29 @@
-﻿clear-host
+Clear-Host
 
-$ErrorActionPreference = "silentlycontinue"
+$ErrorActionPreference = "SilentlyContinue"
 
-$folderlist = @("C$\Program Files (x86)\LANDesk\LDClient\sdmcache","C$\Program Files\LANDesk\LDClient\sdmcache","D$\sdmcache")
+$folderlist = @(
+    "C$\Program Files (x86)\LANDesk\LDClient\sdmcache",
+    "C$\Program Files\LANDesk\LDClient\sdmcache",
+    "D$\sdmcache",
+    "D$\folder"
+)
 
-$computerlist = Get-Content -Path "\\myserver\PowerShell\Servers.txt"
+$excludefolder = "D$\folder"
 
-foreach ($file in $folderlist){
-    foreach ($computer in $computerlist){
-        Write-Host -ForegroundColor Yellow "Deleting contents of $file on $computer"
+$computerlist = Get-Content -Path "\\sysinstalls\supergsrv\HdMatte\PowerShell\Servers.txt"
 
-        $newfilepath = Join-Path "\\$computer\" "$file"
+foreach ($folder in $folderlist) {
+    foreach ($computer in $computerlist) {
+        Write-Host -ForegroundColor Yellow "Deleting contents of $folder on $computer"
+
+        $newfilepath = Join-Path "\\$computer\" $folder
         if (Test-Connection -ComputerName $computer -Quiet) {
-            Get-ChildItem $newfilepath -Recurse -Force -Verbose | Remove-Item -Recurse -Force -Verbose
-            New-Item -ItemType Directory -Force -Path $newfilepath -Verbose | Out-Null
+            if ($folder -ne $excludefolder) {
+                Get-ChildItem $newfilepath -Recurse -Force -Verbose | Remove-Item -Recurse -Force -Verbose
+                Write-Host -ForegroundColor Green "Creating Directory $Folder on $computer"
+                New-Item -ItemType Directory -Force -Path $newfilepath -Verbose | Out-Null
+            }
         } else {
             Write-Warning "Could not connect to $computer"
         }
