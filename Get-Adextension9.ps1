6a478e9cac46a0d0df5a5bfef621839272ea8703
erez669 <57612608+erez669@users.noreply.github.com>
2023-04-27 13:15:00 +0300

    Updated Get-Adextension9.ps1

--- Get-Adextension9.ps1
+++ Get-Adextension9.ps1
@@ -1,52 +1,45 @@
-﻿# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 #                   This script was written by erez schwartz
 # ---------------------------------------------------------------------------
+
+Import-Module ActiveDirectory
 
 $ErrorActionPreference = 'SilentlyContinue'
 
 while ($true) {
-cls
-write-host "edit AD extensionAttribute9 IDnumber"
-write-host "`n" # for creating space
-$Username = Read-Host 'Enter AD UserName' # prompt user to input value
-write-host "`n" # for creating space
-$IDnumber = (Get-ADUser $Username -Properties extensionAttribute9).extensionAttribute9
-$currentID = Read-Host "current IDnumber is $IDnumber do you want to edit? (y/n/r/c) r = remove first 0 / c = Change Existing"
+    Clear-Host
+    Write-Host "`n"
+    Write-Host "Edit AD extensionAttribute9 IDnumber for Synerion`n"
+    $Username = Read-Host 'Enter AD UserName'
+    Write-Host "`n"
+    $IDnumber = (Get-ADUser $Username -Properties extensionAttribute9).extensionAttribute9
+    $currentID = Read-Host "Current IDnumber is $IDnumber. Do you want to edit? (y/n/r/c) r = remove first 0 / c = Change Existing"
 
-if ($currentID -eq 'c')
-{
-write-host "`n" # for creating space
-$newID = Read-Host 'Enter new ID Number'
-Set-ADUser -Identity $Username -replace @{extensionAttribute9 = "$newID"}
-$newValue = $newID -replace "^0"
-Set-ADUser -identity $Username -replace @{extensionAttribute9=$newValue}
-write-host "`n" # for creating space
-Write-Output 'Value Changed Successfully'
-}
-
-if (($currentID -eq 'y') -and ($null -eq $undefinedVariable))
-{
-write-host "`n" # for creating space
-$newID = Read-Host 'Enter ID Number'
-Set-ADUser -Identity $Username -add @{extensionAttribute9 = "$newID"}
-$newValue = $newID -replace "^0"
-Set-ADUser -identity $Username -replace @{extensionAttribute9=$newValue}
-write-host "`n" # for creating space
-Write-Output 'Value added to extensionAttribute9'
-}
-
-if ($currentID -eq 'r')
-{
-$newValue = $IDnumber -replace "^0"
-Set-ADUser -identity $Username -replace @{extensionAttribute9=$newValue}
-write-host "`n" # for creating space
-Write-Output 'the first 0 digit was removed Successfully'
-}
-
-if ($currentID -eq 'n')
-{
-write-host "`n" # for creating space
-Write-Output 'Value not changed'
-}
-Start-Sleep -s 05 # delay command
+    switch ($currentID) {
+        'c' {
+            Write-Host "`n"
+            $newID = Read-Host 'Enter new ID Number'
+            Set-ADUser -Identity $Username -replace @{extensionAttribute9 = $newID}
+            $newValue = $newID -replace "^0"
+            Set-ADUser -identity $Username -replace @{extensionAttribute9 = $newValue}
+            Write-Host "`nValue Changed Successfully`n"
+        }
+        'y' {
+            Write-Host "`n"
+            $newID = Read-Host 'Enter ID Number'
+            Set-ADUser -Identity $Username -add @{extensionAttribute9 = $newID}
+            $newValue = $newID -replace "^0"
+            Set-ADUser -identity $Username -replace @{extensionAttribute9 = $newValue}
+            Write-Host "`nValue added to extensionAttribute9`n"
+        }
+        'r' {
+            $newValue = $IDnumber -replace "^0"
+            Set-ADUser -identity $Username -replace @{extensionAttribute9 = $newValue}
+            Write-Host "`nThe first 0 digit was removed Successfully`n"
+        }
+        'n' {
+            Write-Host "`nValue not changed`n"
+        }
+    }
+    Start-Sleep -Seconds 5
 }
