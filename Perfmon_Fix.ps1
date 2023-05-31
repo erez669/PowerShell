94912a1328f9fa60cb81281c717bda6d384f8c43
erez669 <57612608+erez669@users.noreply.github.com>
2023-05-31 14:19:40 +0300

    Performance Monitor Fix by Enable all Counters
    Performance Monitor Fix by Enable all Counters

---
+++ Perfmon_Fix.ps1
@@ -0,0 +1,16 @@
+﻿$filePath = "C:\install\Counters.txt"
+
+# Export the current performance counter configuration to a file
+lodctr /q > $filePath
+
+# Read the content of the file
+$counters = Get-Content $filePath
+
+# Iterate through each line and enable disabled counters
+$counters | ForEach-Object {
+    if ($_ -match "^\[.*\].*\(Disabled\)$") {
+        $serviceName = ($_ -split "\[|\]")[1]
+        Write-Host "Enabling counter: $serviceName"
+        lodctr "/e:`"$serviceName`""
+    }
+}
