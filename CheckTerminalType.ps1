c1659a07030a73c72e115085177315efceb1bf44
erez669 <57612608+erez669@users.noreply.github.com>
2023-07-03 17:56:08 +0300

    CheckEMV_TerminalType
    CheckEMV_TerminalType

---
+++ CheckTerminalType.ps1
@@ -0,0 +1,39 @@
+﻿# Define the config file paths
+$configFilePaths = @(
+    'C:\retalix\wingpos\MxParams.Config',
+    'C:\Program Files\Retalix\SCO.NET\App\MxParams.Config',
+    'C:\Program Files (x86)\Retalix\SCO.NET\App\MxParams.Config'
+)
+
+# Define the output CSV file path
+$outputCsvFilePath = '\\ivantiappsrv\IvantiShare\Packages\POS\TerminalType\TerminalTypes.csv'
+
+# Get device hostname
+$deviceHostname = [System.Net.Dns]::GetHostName()
+
+# Check if the output CSV file exists. If not, create it and add the header row
+if (!(Test-Path -Path $outputCsvFilePath)) {
+    $headerRow = "HostName,TerminalModel,ConfigFilePath"
+    Add-Content -Path $outputCsvFilePath -Value $headerRow
+}
+
+# Iterate over each config file path
+foreach ($configFilePath in $configFilePaths) {
+    # Skip this iteration if the file does not exist
+    if (!(Test-Path -Path $configFilePath)) {
+        Write-Host "File does not exist: $configFilePath"
+        continue
+    }
+
+    # Load XML content from the .config file
+    [xml]$configData = Get-Content -Path $configFilePath
+
+    # Extract TerminalModel key value pair
+    $terminalModelValue = $configData.configuration.appSettings.add | Where-Object { $_.key -eq 'TerminalModel' } | Select-Object -ExpandProperty value
+
+    # Create a new line for the CSV file
+    $csvLine = "$deviceHostname,$terminalModelValue,$configFilePath"
+
+    # Append the new line to the CSV file
+    Add-Content -Path $outputCsvFilePath -Value $csvLine -Verbose
+}
