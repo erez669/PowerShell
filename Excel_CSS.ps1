19acb5bff0e5f9cbe3415abb45a1a8602cd56c92
erez669 <57612608+erez669@users.noreply.github.com>
2023-07-12 12:06:58 +0300

    Remove unnecessary 0 from IP Address
    Remove unnecessary 0 from IP Address

---
+++ Excel_CSS.ps1
@@ -0,0 +1,12 @@
+﻿Clear-Host
+
+$csv = Import-Csv -Path 'C:\Users\erezsc\Desktop\CSS.csv'
+
+foreach ($row in $csv) {
+    $ipParts = $row.IPAddress -split '\.'
+    $newIpParts = $ipParts | ForEach-Object { [int]$_ }
+    $newIp = $newIpParts -join '.'
+    $row.IPAddress = $newIp
+}
+
+$csv | Export-Csv 'C:\Users\erezsc\Desktop\CSS_Fixed.csv' -NoTypeInformation
