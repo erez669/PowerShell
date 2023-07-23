df959ff086731787a1347d2c1a5f7cecf8c6a70c
erez669 <57612608+erez669@users.noreply.github.com>
2023-07-23 06:49:24 +0300

    Update SQL_Disable_LocalAndRemote.ps1

--- SQL_Disable_LocalAndRemote.ps1
+++ SQL_Disable_LocalAndRemote.ps1
@@ -1,5 +1,21 @@
-# Clear console
+param ([switch]$Elevated)  
+function Test-Admin {  
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())  
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) 
+}  
+
+if ((Test-Admin) -eq $false) {  
+  if ($elevated) {  
+    # tried to elevate, did not work, aborting  
+  } else {  
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file \"{0}\" -elevated' -f ($myinvocation.MyCommand.Definition))  
+  }  
+  exit 
+}  
+
 Clear-Host
+
+'running with full privileges'
 
 # Define services and their names
 $services = @{
