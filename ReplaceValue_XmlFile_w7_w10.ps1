ff843fb604d361ef80c0b6f262964010ee699017
erez669 <57612608+erez669@users.noreply.github.com>
2023-08-19 21:23:28 +0300

    ReplaceValue_XmlFile_w7_w10.ps1

--- ReplaceValue_XmlFile_w7_w10.ps1
+++ ReplaceValue_XmlFile_w7_w10.ps1
@@ -1,19 +1,83 @@
-$computername = $env:COMPUTERNAME
-$name = ($computername -split '-')[1]
-$paths = @('c:\retalix\wingpos\GroceryWinPos.exe.config', 'c:\program files\Retalix\SCO.NET\App\sco.exe.config', 'c:\Program Files (x86)\Retalix\SCO.NET\App\sco.exe.config')
-$value = 'posnlb'
-$replacement = "$value$name"
+# Get computer info
+$computer = Get-WmiObject Win32_ComputerSystem
+$computerName = $computer.Name
+$branchNumber = $computerName.Split('-')[1]
 
-foreach ($path in $paths) {
-    if (Test-Path $path) {
-        $text = [System.IO.File]::ReadAllText($path)
+Write-Host "Computer: $computerName"
+Write-Host "Branch: $branchNumber"
 
-        if ($text -match 'plpossrv') {
-            $text = $text -replace '<URL>http://plpossrv(.*?).posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>', "<URL>http://plpossrv$name.posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>"
-        } else {
-            $text = $text -replace '<URL>http://posnlb(.*?).posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>', "<URL>http://$replacement.posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>"
-        }
+# LDAP group search 
+$ldapPath = "LDAP://OU=Branch Groups,OU=Group Objects,OU=OUs,DC=posprod,DC=supersol,DC=co,DC=il"
+$groupCN = "Branch_$branchNumber"
 
-        [System.IO.File]::WriteAllText($path, $text)
-    }
+Write-Host "Searching LDAP path: $ldapPath"  
+
+$dirEntry = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+$search = New-Object System.DirectoryServices.DirectorySearcher($dirEntry)
+
+$search.Filter = "(CN=$groupCN)"
+$search.PropertiesToLoad.Add("memberOf")
+
+$group = $search.FindOne() 
+
+Write-Host "Number of group memberships found: $($group.Properties.memberof.Count)"
+Write-Host "Found groups:"
+foreach ($groupMembership in $group.Properties.memberof) {
+  $groupName = ($groupMembership -split ',')[0].Split('=')[1]
+  Write-Host "$groupName"
 }
+
+# Target groups
+$targetGroups = @("Net_Supersol_New_Pharm", "Net_SuperSol_Good_Market", "Net_Supersol_Express", "Net_SuperSol_yesh", "Net_SuperSol_yesh_hessed", "Net_SuperSol_Shaarevacha")  
+
+# Check if computer is in target group
+$inTargetGroup = $false
+foreach ($targetGroup in $targetGroups) {
+  if ($group.Properties.memberof -like "*$targetGroup*") {
+    $inTargetGroup = $true
+    break
+  }
+}
+
+Write-Host "In target group: $inTargetGroup"
+
+# Determine config path
+if ($computerName -like '*SCO*') {
+  $configPath1 = 'c:\Program Files\Retalix\SCO.NET\App\sco.exe.config'
+  $configPath2 = 'c:\Program Files (x86)\Retalix\SCO.NET\App\sco.exe.config'
+  $configPath = if (Test-Path $configPath1) { $configPath1 } else { $configPath2 }
+} elseif ($computerName -like '*CSS*') {
+  $configPath = 'c:\Program Files (x86)\Retalix\SCO.NET\App\sco.exe.config'
+} else {
+  $configPath = 'c:\retalix\wingpos\GroceryWinPos.exe.config' 
+}
+
+# Update config
+Write-Host "Updating config: $configPath"
+
+if (Test-Path $configPath) {
+  $text = Get-Content $configPath
+  $newText = @()
+
+  foreach ($line in $text) {
+    if ($line -match '<URL>http://(posnlb|plpossrv)\w*') {
+      if ($inTargetGroup) {
+        $newValue = "http://plpossrv$branchNumber"
+      } else {
+        $newValue = "http://posnlb$branchNumber"
+      }
+      $newLine = $line -replace 'http://(posnlb|plpossrv)\w*', $newValue
+      Write-Host "Replacing URL part with: $newValue"
+    } else {
+      $newLine = $line
+    }
+    $newText += $newLine
+  }
+
+  Set-Content $configPath -Value $newText
+
+} else {
+  Write-Warning "Config file not found: $configPath"
+}
+
+Write-Host "Script complete!"
