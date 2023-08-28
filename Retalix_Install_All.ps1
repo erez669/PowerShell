019606c49f4010472bcef5cc9cb6570499eb0ac0
erez669 <57612608+erez669@users.noreply.github.com>
2023-08-28 08:14:15 +0300

    Retalix_Install_All.ps1
    Retalix_Install_All.ps1

--- Retalix_Install_All.ps1
+++ Retalix_Install_All.ps1
@@ -1,9 +1,9 @@
-﻿# Set verbose preference
+# Set verbose preference
 $VerbosePreference = 'Continue'
 
 # Output file paths
-$outputFile1 = "C:\Install\Run_All_D_output.txt"
-$outputFile2 = "C:\Install\Run_All_C_output.txt"
+$outputFile1 = "C:\Install\RunAll_Servers_output.txt"
+$outputFile2 = "C:\Install\RunAll_Kupot_output.txt"
 
 function Get-OSCaption {
   return (Get-WmiObject Win32_OperatingSystem).Caption 
@@ -22,16 +22,15 @@ $batchArgs = "" # Initialize variable for batch arguments
 # Determine batch file name
 $batchFile = if ($isServer) {
   $batchArgs = $serverArgs # Set args for server
-  "Run_All.cmd"
+  "Run_All_Servers_D.cmd"
 }
 else {
-  "Run_All_C.cmd"
+  "Run_All_Kupot_C.cmd"
 }
 
 Write-Verbose "OS Caption: $osCaption"
 Write-Verbose "Is Server: $isServer" 
 Write-Verbose "Batch file: $batchFile"
-Write-Verbose "Arguments: $batchArgs"
 
 # Write verbose messages to the correct file based on OS type
 if ($isServer) {
@@ -44,13 +43,15 @@ else {
     "OS Caption: $osCaption" | Out-File -FilePath $outputFile2 -Append
     "Is Server: $isServer" | Out-File -FilePath $outputFile2 -Append
     "Batch file: $batchFile" | Out-File -FilePath $outputFile2 -Append
-    "Arguments: $batchArgs" | Out-File -FilePath $outputFile2 -Append
 }
+
+# Get the path of the script
+$scriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 # Call batch file with appropriate args
 if ($isServer) {
-   & "$PSScriptRoot\$batchFile" $batchArgs *>&1 | Out-File -FilePath $outputFile1 -Append # Pass args for server
+   cmd /c "$scriptPath\$batchFile $batchArgs" 2>&1 | Out-File -FilePath $outputFile1 -Append # Pass args for server
 }
 else {
-   & "$PSScriptRoot\$batchFile" *>&1 | Out-File -FilePath $outputFile2 -Append # No args for non-server
+   cmd /c "$scriptPath\$batchFile" 2>&1 | Out-File -FilePath $outputFile2 -Append # No args for non-server
 }
