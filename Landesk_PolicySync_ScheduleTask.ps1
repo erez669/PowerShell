ce1345f2bfe7e6c5f143a849ffcebabcdb3f8840
erez669 <57612608+erez669@users.noreply.github.com>
2023-08-28 11:16:54 +0300

    Landesk ScheduleTask SYSTEM Account
    Landesk ScheduleTask SYSTEM Account

---
+++ Landesk_PolicySync_ScheduleTask.ps1
@@ -0,0 +1,22 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+  Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+  Exit
+}
+
+Clear-Host
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+# Detect OS architecture
+$arch = [System.Environment]::Is64BitOperatingSystem
+$progFiles = if ($arch) { 'C:\Program Files (x86)' } else { 'C:\Program Files' }
+
+# Concatenate the path
+$taskPath = $progFiles + '\LANDesk\LDClient\PolicySync.exe'
+
+# Create the scheduled task
+SCHTASKS /Delete /TN "LANDESK PolicySync" /F
+SCHTASKS /Create /TN "LANDESK PolicySync" /TR "'$taskPath'" /SC HOURLY /MO 3 /RU "NT AUTHORITY\SYSTEM"
+
+# Run the task immediately
+# SCHTASKS /Run /TN "LANDESK PolicySync"
