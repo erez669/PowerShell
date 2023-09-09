cb14e0de1998b4b08ba63b13401e9172e76fca03
erez669 <57612608+erez669@users.noreply.github.com>
2023-09-09 23:06:50 +0300

    EditXML Files
    EditXML Files

---
+++ EditXML_DevicesConfig.ps1
@@ -0,0 +1,41 @@
+﻿$hostname = [System.Environment]::MachineName
+
+if ($hostname -match "^POS-\w+-\w+-SCO$") {
+    if ((Get-WmiObject Win32_OperatingSystem).Version -like "6.1*") {
+        $xmlPath = "C:\Program Files\Retalix\SCO.NET\App\DevicesConfig.xml"  # Windows 7 x86
+    } else {
+        $xmlPath = "C:\Program Files (x86)\Retalix\SCO.NET\App\DevicesConfig.xml"  # Windows 10 x64
+    }
+} elseif ($hostname -match "^POS-\w+-\w+-CSS$") {
+    $xmlPath = "C:\Program Files (x86)\Retalix\SCO.NET\App\DevicesConfig.xml"  # CSS always x64
+} else {
+    Write-Host "Hostname pattern didn't match. Exiting."
+    Exit
+}
+
+if ([string]::IsNullOrEmpty($xmlPath) -or -not (Test-Path $xmlPath)) {
+    Write-Host "XML path is invalid or file does not exist. Exiting."
+    Exit
+}
+
+# Load XML content from file
+$xml = [xml](Get-Content $xmlPath)
+
+# Update the Active attribute for EMV node
+$emvNode = $xml.Devices.EMV
+if ($null -eq $emvNode) {
+    Write-Host "EMV node not found in XML. Exiting."
+    Exit
+}
+
+$oldValue = $emvNode.GetAttribute("Active")
+
+if ($oldValue -eq "true") {
+    Write-Host "Value already true, not changed."
+} else {
+    $emvNode.SetAttribute("Active", "true")
+    Write-Host "Changed EMV Active attribute from $oldValue to true"
+}
+
+# Save changes to the XML file
+$xml.Save($xmlPath)
---
+++ EditXML_EpsonOPOSConfig.ps1
@@ -0,0 +1,43 @@
+﻿# Load XML content from file
+$xmlPath = "C:\retalix\wingpos\Files\Devices\EpsonOPOSConfig.xml"
+$xml = [xml](Get-Content $xmlPath)
+
+# Default setting for EMV
+$emvNode = $xml.POSDevices.POSDeviceList.EMV
+$oldEmvValue = $emvNode.active
+
+# Get physical device model using WMI
+$deviceModel = (Get-WmiObject Win32_ComputerSystemProduct).Name
+
+# Check condition for device models 4852E70 and 4852570
+if ($deviceModel -eq "4852E70" -or $deviceModel -eq "4852570") {
+    if ($oldEmvValue -eq "false") {
+        Write-Host "EMV active attribute already set to false."
+    } else {
+        $emvNode.active = "false"
+        Write-Host "EMV active attribute set to false for physical device model $deviceModel."
+    }
+
+    # Change LineDisplay to active="false"
+    $lineDisplayNode = $xml.POSDevices.POSDeviceList.LineDisplay
+    $oldLineDisplayValue = $lineDisplayNode.active
+
+    if ($oldLineDisplayValue -eq "false") {
+        Write-Host "LineDisplay active attribute already set to false."
+    } else {
+        $lineDisplayNode.active = "false"
+        Write-Host "LineDisplay active attribute set to false for physical device model $deviceModel."
+    }
+} else {
+    if ($oldEmvValue -eq "true") {
+        Write-Host "EMV active attribute already set to true."
+    } else {
+        $emvNode.active = "true"
+        Write-Host "EMV active attribute set to true."
+    }
+
+    Write-Host "No change was made to LineDisplay value."
+}
+
+# Save changes to the XML file
+$xml.Save($xmlPath)
