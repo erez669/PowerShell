e8f0ac7c82e784f5d784a1b10deb03c9b82ae79f
erez669 <57612608+erez669@users.noreply.github.com>
2023-09-18 21:13:23 +0300

    Landesk_PolicySync_ScheduleTask.ps1
    Landesk_PolicySync_ScheduleTask.ps1

--- Landesk_PolicySync_ScheduleTask.ps1
+++ Landesk_PolicySync_ScheduleTask.ps1
@@ -1,35 +1,30 @@
-# Initialize log file
-$logFile = "C:\Install\PolicySync_log.txt"
-
-# Function to append to log
-function Write-Log {
-    Param (
-        [string]$message
-    )
-    Add-Content $logFile -Value "$(Get-Date) - $message"
-}
-
-# Check for Admin privileges
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-    Write-Log "Not running as an admin. Attempting to run as admin."
-    $scriptPath = $MyInvocation.MyCommand.Path
-    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
+# Automatically relaunch as admin if not already
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Start-Process powershell.exe -Verb RunAs -ArgumentList "-File", ('"{0}"' -f $MyInvocation.MyCommand.Path)
     exit
 }
 
-# Detect OS architecture
+$TaskScheduler = New-Object -ComObject "Schedule.Service"
+$TaskScheduler.Connect()
+$RootFolder = $TaskScheduler.GetFolder("\")
+$TaskDefinition = $TaskScheduler.NewTask(0)
+$Action = $TaskDefinition.Actions.Create(0)
+
 $arch = [System.Environment]::Is64BitOperatingSystem
-$progFiles = if ($arch) { 'C:\Program Files (x86)' } else { 'C:\Program Files' }
-$taskPath = Join-Path $progFiles 'LANDesk\LDClient\PolicySync.exe'
+$Action.Path = if ($arch) { 'C:\Program Files (x86)\LANDesk\LDClient\PolicySync.exe' } else { 'C:\Program Files\LANDesk\LDClient\PolicySync.exe' }
 
-# Check if the task exists
-$taskExist = & SCHTASKS /Query /TN "LANDESK PolicySync" 2>&1
-if ($taskExist -like "*LANDESK PolicySync*") {
-    Write-Log "Task exists. Deleting."
-    $deleteResult = & SCHTASKS /Delete /TN "LANDESK PolicySync" /F 2>&1
-    Write-Log "Delete Result: $deleteResult"
-}
+$TaskDefinition.Principal.RunLevel = 1 # Highest privileges
+$Trigger = $TaskDefinition.Triggers.Create(1)  # Time trigger
+$StartTime = (Get-Date).AddDays(1).Date.AddHours(2)  # Starts at 2 AM tomorrow
+$Trigger.StartBoundary = $StartTime.ToString("yyyy-MM-ddTHH:mm:ss")
+$Trigger.Repetition.Interval = "PT2H"  # Interval is 2 hours
+$Trigger.Enabled = $true
+$Settings = $TaskDefinition.Settings
+$Settings.AllowDemandStart = $true
+$Settings.DisallowStartIfOnBatteries = $false
+$Settings.StopIfGoingOnBatteries = $false
+$Settings.WakeToRun = $true
+$Settings.ExecutionTimeLimit = "PT0S"
+$Settings.StartWhenAvailable = $true
 
-# Create the task
-$createResult = & SCHTASKS /Create /TN "LANDESK PolicySync" /TR "$taskPath" /SC HOURLY /ST 02:00 /RU "NT AUTHORITY\SYSTEM" /MO 2 2>&1
-Write-Log "Create Result: $createResult"
+$RootFolder.RegisterTaskDefinition("LANDESK PolicySync", $TaskDefinition, 6, "NT AUTHORITY\SYSTEM", $null, 5)
