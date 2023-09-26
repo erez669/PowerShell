1fc7a8e60e4c308258bf5852824f21b7b9796bf5
erez669 <57612608+erez669@users.noreply.github.com>
2023-09-26 09:38:06 +0300

    EditXML_EpsonOPOSConfig.ps1
    EditXML_EpsonOPOSConfig.ps1

--- EditXML_EpsonOPOSConfig.ps1
+++ EditXML_EpsonOPOSConfig.ps1
@@ -1,43 +1,63 @@
-﻿# Load XML content from file
+# Load XML content from file
+Write-Host "Loading XML file..."
 $xmlPath = "C:\retalix\wingpos\Files\Devices\EpsonOPOSConfig.xml"
 $xml = [xml](Get-Content $xmlPath)
 
-# Default setting for EMV
+# Get EMV Node
+Write-Host "Retrieving EMV settings..."
 $emvNode = $xml.POSDevices.POSDeviceList.EMV
 $oldEmvValue = $emvNode.active
+Write-Host "Old EMV value: $oldEmvValue"
+
+# Always set EMV to true
+Write-Host "Setting EMV to true..."
+$emvNode.active = "true"
+
+# Reload the value
+$emvNode = $xml.POSDevices.POSDeviceList.EMV
+$newEmvValue = $emvNode.active
+Write-Host "New EMV value after running is: $newEmvValue"
 
 # Get physical device model using WMI
+Write-Host "Fetching device model..."
 $deviceModel = (Get-WmiObject Win32_ComputerSystemProduct).Name
+Write-Host "Device Model: $deviceModel"
 
-# Check condition for device models 4852E70 and 4852570
-if ($deviceModel -eq "4852E70" -or $deviceModel -eq "4852570") {
-    if ($oldEmvValue -eq "false") {
-        Write-Host "EMV active attribute already set to false."
-    } else {
-        $emvNode.active = "false"
-        Write-Host "EMV active attribute set to false for physical device model $deviceModel."
-    }
-
-    # Change LineDisplay to active="false"
+# Update LineDisplay for specific device models
+$specialModels = @("4852E70", "4852570", "6200E35")
+if ($specialModels -contains $deviceModel) {
+    Write-Host "Special model detected, updating LineDisplay..."
     $lineDisplayNode = $xml.POSDevices.POSDeviceList.LineDisplay
-    $oldLineDisplayValue = $lineDisplayNode.active
-
-    if ($oldLineDisplayValue -eq "false") {
-        Write-Host "LineDisplay active attribute already set to false."
-    } else {
-        $lineDisplayNode.active = "false"
-        Write-Host "LineDisplay active attribute set to false for physical device model $deviceModel."
-    }
-} else {
-    if ($oldEmvValue -eq "true") {
-        Write-Host "EMV active attribute already set to true."
-    } else {
-        $emvNode.active = "true"
-        Write-Host "EMV active attribute set to true."
-    }
-
-    Write-Host "No change was made to LineDisplay value."
+    $lineDisplayNode.active = "false"
 }
 
-# Save changes to the XML file
+# LDAP group search
+$computer = Get-WmiObject Win32_ComputerSystem
+$branchNumber = $computer.Name.Split('-')[1]
+$ldapPath = "LDAP://OU=Branch Groups,OU=Group Objects,OU=OUs,DC=posprod,DC=supersol,DC=co,DC=il"
+$groupCN = "Branch_$branchNumber"
+
+$dirEntry = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+$search = New-Object System.DirectoryServices.DirectorySearcher($dirEntry)
+$search.Filter = "(CN=$groupCN)"
+$search.PropertiesToLoad.Add("memberOf")
+
+$group = $search.FindOne()
+
+# Update Scale if in Net_Supersol_New_Pharm
+if ($group.Properties.memberof -like "*Net_Supersol_New_Pharm*") {
+    $scaleNode = $xml.POSDevices.POSDeviceList.Scale
+    $oldScaleValue = $scaleNode.active
+    Write-Host "Old Scale value: $oldScaleValue"
+
+    Write-Host "Net_Supersol_New_Pharm group detected, updating Scale value to false..."
+    $scaleNode.active = "false"
+    $scaleNode = $xml.POSDevices.POSDeviceList.Scale
+    $newScaleValue = $scaleNode.active
+    Write-Host "New Scale value after running is: $newScaleValue"
+}
+
+# Save XML changes
+Write-Host "Saving changes to XML file..."
 $xml.Save($xmlPath)
+Write-Host "Done."
