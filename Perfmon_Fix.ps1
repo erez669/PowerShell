31302268d5415f71fdc6bd6dd37f33d059bad8ef
erez669 <57612608+erez669@users.noreply.github.com>
2023-09-28 14:07:08 +0300

    Performance_Monitor_Fix.ps1
    Performance_Monitor_Fix.ps1

--- Perfmon_Fix.ps1
+++ Perfmon_Fix.ps1
@@ -1,15 +1,15 @@
-﻿$filePath = "C:\install\Counters.txt"
+$filePath = "C:\install\Counters.txt"
 
 # Export the current performance counter configuration to a file
-lodctr /q > $filePath
+lodctr /q | Out-File -FilePath $filePath
 
 # Read the content of the file
 $counters = Get-Content $filePath
 
 # Iterate through each line and enable disabled counters
-$counters | ForEach-Object {
-    if ($_ -match "^\[.*\].*\(Disabled\)$") {
-        $serviceName = ($_ -split "\[|\]")[1]
+foreach ($counter in $counters) {
+    if ($counter -match "^\[.*\].*\(Disabled\)$") {
+        $serviceName = ($counter -split "\[|\]")[1]
         Write-Host "Enabling counter: $serviceName"
         lodctr "/e:`"$serviceName`""
     }
