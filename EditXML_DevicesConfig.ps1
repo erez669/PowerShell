e932b9c67837c6a4c4731318ba7979ef8322001b
erez669 <57612608+erez669@users.noreply.github.com>
2023-09-29 20:39:11 +0300

    EditXML_DevicesConfig.ps1
    EditXML_DevicesConfig.ps1

--- EditXML_DevicesConfig.ps1
+++ EditXML_DevicesConfig.ps1
@@ -1,8 +1,8 @@
 $hostname = [System.Environment]::MachineName
 
-if ($hostname -match "^POS-\d{3}-\d{2}-SCO$") {
+if ($hostname -match "^POS-\d{3}-\d{2}-(SCO|WAN|MOD)$") {
 
-  Write-Host "Debug: Matched POS-XYZ-XX-SCO hostname format detected, continue with script execution"
+  Write-Host "Debug: Matched POS-XYZ-XX-SCO/WAN/MOD hostname format detected, continue with script execution"
 
   if ((Get-WmiObject Win32_OperatingSystem).Version -like "6.1*") {
     $xmlPath = "C:\Program Files\Retalix\SCO.NET\App\DevicesConfig.xml" 
@@ -10,16 +10,16 @@ if ($hostname -match "^POS-\d{3}-\d{2}-SCO$") {
     $xmlPath = "C:\Program Files (x86)\Retalix\SCO.NET\App\DevicesConfig.xml"
   }
 
-} elseif ($hostname -match "^POS-\d{3}-\d{2}-CSS$") {
+} elseif ($hostname -match "^POS-\d{3}-\d{2}-(CSS|WAN|MOD)$") {
 
-  Write-Host "Debug: Matched POS-XYZ-XX-CSS hostname format detected, continue with script execution"
+  Write-Host "Debug: Matched POS-XYZ-XX-CSS/WAN/MOD hostname format detected, continue with script execution"
   
   $xmlPath = "C:\Program Files (x86)\Retalix\SCO.NET\App\DevicesConfig.xml"
 
 } else {
 
   Write-Host "Debug: Hostname format did not match, script execution was stopped"
-  Exit 8
+  Exit 9
 
 }
 
@@ -31,6 +31,42 @@ if ([string]::IsNullOrEmpty($xmlPath) -or -not (Test-Path $xmlPath)) {
 # Load XML content from file
 $xml = [xml](Get-Content $xmlPath)
 Write-Host "XML loaded from: $xmlPath"
+
+# LDAP group search
+$computer = Get-WmiObject Win32_ComputerSystem
+$branchNumber = $computer.Name.Split('-')[1]
+$ldapPath = "LDAP://OU=Branch Groups,OU=Group Objects,OU=OUs,DC=posprod,DC=supersol,DC=co,DC=il"
+$groupCN = "Branch_$branchNumber"
+Write-Host "Branch number is $branchNumber"
+
+$dirEntry = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+$search = New-Object System.DirectoryServices.DirectorySearcher($dirEntry)
+$search.Filter = "(CN=$groupCN)"
+[void]$search.PropertiesToLoad.Add("memberOf")
+
+$group = $search.FindOne()
+
+if ($null -ne $group) {
+    $groupMembers = $group.Properties.memberof | ForEach-Object {
+        if ($_ -match 'CN=([^,]+)') {
+            $matches[1]
+        }
+    } | Where-Object { $_ -like 'NET_*' }
+    $groupMembers = $groupMembers -join ', '
+    Write-Host "LDAP Group detected: $groupMembers"
+}
+
+# Update Scale if in Net_Supersol_New_Pharm
+if ($group.Properties.memberof -like "*Net_Supersol_New_Pharm*") {
+    $scaleNode = $xml.Devices.Scale
+    if ($null -eq $scaleNode) {
+        Write-Host "Scale node not found in XML. Exiting."
+        Exit
+    }
+
+    $scaleNode.SetAttribute("Active", "false")
+    Write-Host "Changed Scale Active attribute to false"
+}
 
 # Update the Active attribute for EMV node
 $emvNode = $xml.Devices.EMV
@@ -54,5 +90,5 @@ Write-Host "XML update completed successfully!"
 # Save changes to the XML file
 $xml.Save($xmlPath)
 
-# Output file path 
+# Output file path
 Write-Host "Updated XML saved to: $xmlPath"
