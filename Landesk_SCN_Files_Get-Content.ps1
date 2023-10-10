35a098905faca423bd14f4e9578b0ea1ca348cba
erez669 <57612608+erez669@users.noreply.github.com>
2023-10-10 15:16:05 +0300

    Landesk_SCN_Files_Get-Content.ps1
    Landesk_SCN_Files_Get-Content.ps1

---
+++ Landesk_SCN_Files_Get-Content.ps1
@@ -0,0 +1,22 @@
+$directory = "E:\LANDesk\ManagementSuite\ldscan\ErrorScan"
+$outputFile = "E:\LANDesk\ManagementSuite\ldscan\ErrorScan\OutPut-scn.csv"
+
+$deviceNames = @()
+
+Get-ChildItem -Path $directory -Filter *.scn | ForEach-Object {
+    Write-Host "Processing file: $($_.FullName)"
+    $content = Get-Content $_.FullName -Raw
+    if ($content -match 'Device Name =(.+?)\r?\n') {
+        $deviceName = $matches[1].Trim()
+        Write-Host "Device Name found: $deviceName"
+        $deviceNames += [PSCustomObject]@{
+            DeviceName = $deviceName
+        }
+    }
+    else {
+        Write-Host "No Device Name found in $($_.FullName)"
+    }
+}
+
+$deviceNames | Export-Csv -Path $outputFile -NoTypeInformation
+Write-Host "Exported to $outputFile"
