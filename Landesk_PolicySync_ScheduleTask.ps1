e393b7795efb4149c5c7615ab47d7768f376e1fe
erez669 <57612608+erez669@users.noreply.github.com>
2023-10-12 18:25:59 +0300

    Landesk_PolicySync_ScheduleTask.ps1
    Landesk_PolicySync_ScheduleTask.ps1

--- Landesk_PolicySync_ScheduleTask.ps1
+++ Landesk_PolicySync_ScheduleTask.ps1
@@ -11,52 +11,53 @@ function Write-Log {
     }
 }
 
-# Automatically relaunch as admin if not already
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-    Write-Log "Not running as administrator. Relaunching as admin..."
-    Start-Process powershell.exe -Verb RunAs -ArgumentList "-File", ('"{0}"' -f $MyInvocation.MyCommand.Path)
-    exit
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
 }
 
-$TaskScheduler = New-Object -ComObject "Schedule.Service"
-$TaskScheduler.Connect()
-$RootFolder = $TaskScheduler.GetFolder("\")
-$TaskName = "LANDESK PolicySync"
+Clear-Host
 
-# Check if the task exists
-try {
-    $ExistingTask = $RootFolder.GetTask($TaskName)
-    if ($ExistingTask) {
-        Write-Log "Task '$TaskName' already exists. Deleting..."
-        $RootFolder.DeleteTask($TaskName, 0)
-    }
+$VerbosePreference = 'Continue'
+
+$taskName = "LANDesk_PolicySync_Workaround"
+
+$taskService = New-Object -ComObject Schedule.Service
+$taskService.Connect($env:COMPUTERNAME)
+
+$rootFolder = $taskService.GetFolder("\")
+$existingTasks = $rootFolder.GetTasks(0)
+
+# If the old task exists, delete it
+$oldTask = $existingTasks | Where-Object { $_.Name -eq $taskName }
+if ($oldTask) {
+    $rootFolder.DeleteTask("\$taskName", 0)
 }
-catch {
-    Write-Log "Task '$TaskName' does not exist."
-}
 
-$TaskDefinition = $TaskScheduler.NewTask(0)
-$Action = $TaskDefinition.Actions.Create(0)
+$taskDefinition = $taskService.NewTask(0)
 
-$arch = [System.Environment]::Is64BitOperatingSystem
-$Action.Path = if ($arch) { 'C:\Program Files (x86)\LANDesk\LDClient\PolicySync.exe' } else { 'C:\Program Files\LANDesk\LDClient\PolicySync.exe' }
+$action = $taskDefinition.Actions.Create(0)
+$action.Path = "C:\Windows\System32\cmd.exe"
+$action.Arguments = '/C start /min powershell.exe -File "\\HDTOOLSSRV\pos\LandeskPolicy_Sync_Workaround.ps1"'
 
-$TaskDefinition.Principal.RunLevel = 1 # Highest privileges
-$Trigger = $TaskDefinition.Triggers.Create(1)  # Time trigger
-$StartTime = (Get-Date).AddDays(1).Date.AddHours(2)  # Starts at 2 AM tomorrow
-$Trigger.StartBoundary = $StartTime.ToString("yyyy-MM-ddTHH:mm:ss")
-$Trigger.Repetition.Interval = "PT2H"  # Interval is 2 hours
-$Trigger.Enabled = $true
-$Settings = $TaskDefinition.Settings
-$Settings.AllowDemandStart = $true
-$Settings.DisallowStartIfOnBatteries = $false
-$Settings.StopIfGoingOnBatteries = $false
-$Settings.WakeToRun = $true
-$Settings.ExecutionTimeLimit = "PT0S"
-$Settings.StartWhenAvailable = $true
+$triggers = $taskDefinition.Triggers
+$trigger = $triggers.Create(1)
+$trigger.StartBoundary = (Get-Date).AddDays(1).ToString('yyyy-MM-dd') + 'T00:00:00'
+$trigger.Repetition.Interval = 'PT6H'
 
-# Register the task
-$RootFolder.RegisterTaskDefinition($TaskName, $TaskDefinition, 6, "NT AUTHORITY\SYSTEM", $null, 5)
+$principal = $taskDefinition.Principal
+$principal.GroupId = 'S-1-5-18'
+$principal.RunLevel = 1
+
+$settings = $taskDefinition.Settings
+$settings.DisallowStartIfOnBatteries = $false
+$settings.StartWhenAvailable = $true
+$settings.WakeToRun = $true
+$settings.ExecutionTimeLimit = 'PT0S'
+
+$rootFolder.RegisterTaskDefinition($taskName, $taskDefinition, 6, $null, $null, 3)
 
 Write-Log "Task '$TaskName' created successfully."
 
