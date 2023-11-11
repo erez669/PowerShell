4fbece9179ae31bcea75a93d945ab197fbfd6d29
erez669 <57612608+erez669@users.noreply.github.com>
2023-11-11 22:26:33 +0200

    BackupFolders_v2_ProgressBar.ps1
    BackupFolders_v2_ProgressBar.ps1
    
    Yarpa

--- BackupFolders_v2_ProgressBar.ps1
+++ BackupFolders_v2_ProgressBar.ps1
@@ -1,44 +1,69 @@
+# Define the path for local backup files
 $localFiles = "C:\yarpadb\sql_Backup"
-$files = Get-ChildItem -Path $localFiles -Recurse
 
-# Extract the last three characters from the hostname
-$serverNum = $env:COMPUTERNAME -match '\d{3}$'
-if ($Matches[0]) {
+# Get today's date
+$today = (Get-Date).Date
+
+# Get only .bak files modified today
+$files = Get-ChildItem -Path $localFiles -Filter *.bak -Recurse -Verbose | Where-Object { $_.LastWriteTime -gt $today }
+
+# Extract the appropriate number from the hostname
+if ($env:COMPUTERNAME -match 'WKS-(\d+)-\d{3}') {
+    # If hostname follows the WKS-xxx-yyy pattern, extract the middle number (xxx)
+    $serverNum = $Matches[1]
+} elseif ($env:COMPUTERNAME -match '\d{3}$') {
+    # Otherwise, if hostname ends with three digits, extract these
     $serverNum = $Matches[0]
-    $remotePathPL = "\\PLPOSSRV$serverNum\E$\YarpaBackup_$serverNum"
-    $remotePathWKS = "\\WKS-$serverNum-102\C$\YarpaBackup_$serverNum"
+} else {
+    Write-Host "The hostname does not match expected patterns."
+    exit
+}
 
-    Write-Host "Server number detected: $serverNum"
+Write-Host "Snif number detected: $serverNum"
 
-    # Function to copy files with progress
-    function Copy-WithProgress($source, $destination) {
-        $fileCount = $files.Count
-        $currentFile = 0
-        
-        foreach ($file in $files) {
-            $currentFile++
-            $percentComplete = ($currentFile / $fileCount) * 100
-            Write-Progress -Activity "Copying files to $destination" -Status "$currentFile of $fileCount" -PercentComplete $percentComplete
-            
-            $destinationPath = $file.FullName.Replace($source, $destination)
-            $destinationDir = [System.IO.Path]::GetDirectoryName($destinationPath)
-            
-            if (-not (Test-Path $destinationDir)) {
-                New-Item -ItemType Directory -Path $destinationDir | Out-Null
-            }
-            
-            Copy-Item -Path $file.FullName -Destination $destinationPath -Force -Verbose
+$remotePathPL = "\\PLPOSSRV$serverNum\E$\YarpaBackup_$serverNum"
+$remotePathWKS = "\\WKS-$serverNum-102\C$\YarpaBackup_$serverNum"
+
+# Function to copy files with progress and remove old backups
+function Copy-WithProgress($source, $destination, $filesToCopy) {
+    # Try to remove old .bak files from the destination, handling errors gracefully
+    $existingBackups = Get-ChildItem -Path $destination -Filter *.bak -Recurse -ErrorAction SilentlyContinue
+    foreach ($backup in $existingBackups) {
+        try {
+            Remove-Item $backup.FullName -Force -ErrorAction Stop
+        } catch {
+            Write-Host "Could not remove file: $($backup.FullName). Error: $_"
         }
-        
-        Write-Progress -Activity "Copying files to $destination" -Completed
     }
 
+    $fileCount = $filesToCopy.Count
+    $currentFile = 0
+
+    foreach ($file in $filesToCopy) {
+        $currentFile++
+        $percentComplete = ($currentFile / $fileCount) * 100
+        Write-Progress -Activity "Copying files to $destination" -Status "$currentFile of $fileCount" -PercentComplete $percentComplete
+
+        $destinationPath = $file.FullName.Replace($source, $destination)
+        $destinationDir = [System.IO.Path]::GetDirectoryName($destinationPath)
+
+        if (-not (Test-Path $destinationDir)) {
+            New-Item -ItemType Directory -Path $destinationDir | Out-Null
+        }
+
+        Copy-Item -Path $file.FullName -Destination $destinationPath
+    }
+
+    Write-Progress -Activity "Copying files to $destination" -Completed
+}
+
+# Check if there are files to copy
+if ($files.Count -gt 0) {
     # Copy the files to the remote servers
-    Copy-WithProgress -source $localFiles -destination $remotePathPL
-    Copy-WithProgress -source $localFiles -destination $remotePathWKS
+    Copy-WithProgress -source $localFiles -destination $remotePathPL -filesToCopy $files
+    Copy-WithProgress -source $localFiles -destination $remotePathWKS -filesToCopy $files
 
     Write-Host "Files copied successfully."
-
 } else {
-    Write-Host "The hostname does not end with three digits."
+    Write-Host "No new .bak files to copy today."
 }
