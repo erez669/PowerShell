8f7d6b2235443880c2df0cbddf8cad71b4dbdddf
erez669 <57612608+erez669@users.noreply.github.com>
2023-11-25 01:34:06 +0200

    Saturday_Reboot.ps1
    Saturday_Reboot.ps1

--- Saturday_Reboot.ps1
+++ Saturday_Reboot.ps1
@@ -12,8 +12,8 @@ $today = Get-Date
 Write-Host "Today: $($today.ToString('dd/MM/yyyy'))" -ForegroundColor Yellow
 
 # Specify the source and destination paths for the .ps1 and .xml files
-$ps1SourcePath = "\\ivantiappsrv.corp.supersol.co.il\IvantiShare\Packages\Servers_PL_SL\Maintanance\Reboot\saturday_Reboot.ps1"
-$ps1DestinationPath = "C:\Install\saturday_Reboot.ps1"
+$ps1SourcePath = "\\ivantiappsrv.corp.supersol.co.il\IvantiShare\Packages\Servers_PL_SL\Maintanance\Reboot\Saturday_Reboot.ps1"
+$ps1DestinationPath = "C:\Install\Saturday_Reboot.ps1"
 $xmlSourcePath = "\\ivantiappsrv.corp.supersol.co.il\IvantiShare\Packages\Servers_PL_SL\Maintanance\Reboot\Reboot_Server.xml"
 $xmlDestinationPath = "C:\Install\Reboot_Server.xml"
 
@@ -24,8 +24,8 @@ $folderPath = "C:\Install"
 New-Item -Path $folderPath -ItemType Directory -ErrorAction SilentlyContinue -Verbose
 
 # Copy the .ps1 and .xml files
-Copy-Item -Path $ps1SourcePath -Destination $ps1DestinationPath -Verbose
-Copy-Item -Path $xmlSourcePath -Destination $xmlDestinationPath -Verbose
+Copy-Item -Path $ps1SourcePath -Destination $ps1DestinationPath -Verbose -Force
+Copy-Item -Path $xmlSourcePath -Destination $xmlDestinationPath -Verbose -Force
 
 # Determine the week number of the month
 $weekNum = [math]::Ceiling($today.Day / 7)
@@ -34,21 +34,24 @@ $weekNum = [math]::Ceiling($today.Day / 7)
 $hostname = $env:COMPUTERNAME
 
 function Get-LastSaturday($year, $month) {
-    $firstDayOfNextMonth = Get-Date -Year $year -Month $month -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0
-    $firstDayOfNextMonth = $firstDayOfNextMonth.AddMonths(1)
-    $lastDayOfMonth = $firstDayOfNextMonth.AddDays(-1)
+    $lastDayOfMonth = Get-Date -Year $year -Month $month -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0
+    $lastDayOfMonth = $lastDayOfMonth.AddMonths(1).AddDays(-1)
 
-    $daysUntilSaturday = [DayOfWeek]::Saturday.value__ - $lastDayOfMonth.DayOfWeek.value__
-    if ($daysUntilSaturday -lt 0) { $daysUntilSaturday += 7 }
-    $lastSaturday = $lastDayOfMonth.AddDays(-$daysUntilSaturday)
+    while ($lastDayOfMonth.DayOfWeek -ne [DayOfWeek]::Saturday) {
+        $lastDayOfMonth = $lastDayOfMonth.AddDays(-1)
+    }
 
-    return $lastSaturday
+    return $lastDayOfMonth
 }
+
+# Calculate the last Saturday of the current month
+$lastSaturday = Get-LastSaturday -Year $today.Year -Month $today.Month
 
 # Check if today is Saturday
 if ($today.DayOfWeek -eq 'Saturday') {
     # Determine which Saturday of the month it is and check hostname to decide whether to reboot
     switch ($weekNum) {
+
         1 { 
             if ($hostname -like '*PLPOSSRV*') {
                 Write-Verbose "Rebooting on the first Saturday of the month"
@@ -68,16 +71,28 @@ if ($today.DayOfWeek -eq 'Saturday') {
             }
         }
         default {
-            # Calculate the last Saturday of the current month once
-            $lastSaturday = Get-LastSaturday -Year $today.Year -Month $today.Month
+            Write-Verbose "Checking for the last Saturday"
 
             # Check if today is the last Saturday and the hostname pattern matches
-            if ($today.Date.Equals($lastSaturday.Date) -and $hostname -like '*YARPASQL*') {
-                Write-Verbose "Rebooting on the last Saturday of the month"
-                shutdown -r -t 0 -f
+            if ($today.Date -eq $lastSaturday.Date) {
+                Write-Verbose "Today is the last Saturday of the month"
+
+                if ($hostname -like '*YARPASQL*') {
+                    Write-Verbose "Hostname matches. Initiating reboot."
+                    shutdown -r -t 0 -f
+                }
+                else {
+                    Write-Verbose "Hostname does not match. No reboot."
+                }
+            }
+            else {
+                Write-Verbose "Today is not the last Saturday. No reboot."
             }
         }
     }
+}
+else {
+    Write-Verbose "Today is not Saturday. No reboot."
 }
 
 schtasks /create /tn "Reboot_Server" /xml "C:\Install\Reboot_Server.xml" /F
