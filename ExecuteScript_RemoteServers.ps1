675a6d48ae332cfe76544f9b5fc45ce2f46fc2d9
erez669 <57612608+erez669@users.noreply.github.com>
2024-03-13 09:10:20 +0200

    Run PS1 on Remote Servers
    Run PS1 on Remote Servers

---
+++ ExecuteScript_RemoteServers.ps1
@@ -0,0 +1,15 @@
+﻿$servers = Get-Content '\\10.251.10.251\supergsrv\HdMatte\PowerShell\Servers.txt'
+
+foreach ($server in $servers) {
+    # Establish a session for each server
+    $session = New-PSSession -ComputerName $server
+    
+    # Execute the script on the D: partition of the server
+    Invoke-Command -Session $session -ScriptBlock {
+        Set-Location D:\
+        .\DeleteFolders_SDMCACHE_v2.ps1
+    }
+
+    # Close the session
+    Remove-PSSession -Session $session
+}
